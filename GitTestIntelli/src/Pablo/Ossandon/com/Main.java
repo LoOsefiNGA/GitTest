2606347f0a00d24b738ca98ff71729eeec58e5f8
Pablo Ossandon <Pablo__ossandon@hotmail.se>
2017-03-30 14:27:17 +0200

    GitTest

---
+++ GitTestIntelli/src/Pablo/Ossandon/com/Main.java
@@ -0,0 +1,16 @@
+package Pablo.Ossandon.com;
+
+public class Main {
+
+    public void wasUp(){
+        System.out.println("Waaazzzz Up!!!");
+    }
+
+    public static void main(String[] args) {
+
+	Main GitTest = new Main();
+
+	GitTest.wasUp();
+
+    }
+}
